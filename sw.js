5be68da243820e8f6f25363c924c432f1713446b
carluk13 <carlossancho@usal.es>
2026-09-23 15:55:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Gym · service worker
-var VERSION = '202609231148-6c834063';
+var VERSION = '202609231351-633675b8';
 var CORE = 'gym-core-' + VERSION;
 var FILES = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 self.addEventListener('install', function (e) { e.waitUntil(caches.open(CORE).then(function (c) { return c.addAll(FILES); }).then(function () { return self.skipWaiting(); })); });
